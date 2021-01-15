dbbd0183715300869bf8d89152cdd2c80ba72746
uhhhsoyan <eric.essoyan@gmail.com>
2021-01-15 14:06:53 -0800

    catching up server side

---
+++ controllers/posts.js
@@ -0,0 +1,71 @@
+import mongoose from 'mongoose';
+import PostMessage from '../models/postMessage.js';
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+
+  } catch (error) {
+    res.status(409).json({ message: error.message })
+  }
+}
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params; // renaming id to _id
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+
+  res.json(updatedPost);
+
+}
+
+export const deletePost = async (req, res) => {
+  const { id: _id } = req.params;
+  
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+  const deletedPost = await PostMessage.findByIdAndRemove(_id);
+
+  res.json({ message: 'Post deleted successfully'});
+}
+
+export const likePost = async (req, res) => {
+  const { id: _id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+  const post = await PostMessage.findById(_id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, { likeCount: post.likeCount + 1 }, { new: true })
+
+  res.json(updatedPost);
+}
+
+// export const createPost = async (req, res) => {
+//   const { title, message, selectedFile, creator, tags } = req.body;
+
+//   const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
+
+//   try {
+//       await newPostMessage.save();
+
+//       res.status(201).json(newPostMessage );
+//   } catch (error) {
+//       res.status(409).json({ message: error.message });
+//   }
+// }
